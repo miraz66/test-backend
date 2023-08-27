cc0fcaa90ed3cbaa929b705ab4c4a196a9a6faac
miraz66 <mirajulislam0509@gmail.com>
2023-08-27 23:16:57 +0600

    images Modal

--- src/components/PopUpImage.jsx
+++ src/components/PopUpImage.jsx
@@ -40,8 +40,6 @@ export default function PopUpImage({ imgID }) {
         <div className=""></div>
         <div className=""></div>
         <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
       </div>
     </>
   );
