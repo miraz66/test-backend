182dba46ab504e3f65a4a54803beddac154d2f2d
miraz66 <mirajulislam0509@gmail.com>
2023-08-27 23:20:03 +0600

    Test

--- src/components/PopUpImage.jsx
+++ src/components/PopUpImage.jsx
@@ -27,8 +27,6 @@ export default function PopUpImage({ imgID }) {
         <div className=""></div>
         <div className=""></div>
         <div className=""></div>
-        <div className=""></div>
-        <div className=""></div>
       </div>
     </>
   );
