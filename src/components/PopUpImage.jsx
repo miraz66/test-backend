62e22f19b88ad6c4c40da9e07500d26dc74e7f87
miraz66 <mirajulislam0509@gmail.com>
2023-08-29 23:50:22 +0600

    CardTest

--- src/components/PopUpImage.jsx
+++ src/components/PopUpImage.jsx
@@ -58,6 +58,8 @@ export default function PopUpImage({ imgID }) {
         <div className="">dsadasd</div>
         <div className="">dsadasd</div>
         <div className="">dsadasd</div>
+        <div className="">dsadasd</div>
+        <div className="">dsadasd</div>
       </div>
     </>
   );
