80cdfb45b74e0b8e682213b1262c43af0b7353fd
miraz66 <mirajulislam0509@gmail.com>
2023-08-29 23:48:37 +0600

    Conform

--- src/components/PopUpImage.jsx
+++ src/components/PopUpImage.jsx
@@ -40,6 +40,8 @@ export default function PopUpImage({ imgID }) {
         <div className="">dsadasd</div>
         <div className="">dsadasd</div>
         <div className="">dsadasd</div>
+        <div className="">dsadasd</div>
+        <div className="">dsadasd</div>
       </div>
     </>
   );
