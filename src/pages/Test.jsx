b857086432ef76cc49d3bdeec2eba55ba5bb72a7
miraz66 <mirajulislam0509@gmail.com>
2023-09-08 23:54:09 +0600

    Animation

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -29,10 +29,6 @@ export default function Test() {
           <div className="">sdfsdf</div>
           <div className="">sdfsdf</div>
           <div className="">sdfsdf</div>
-          <div className="">sdfsdf</div>
-          <div className="">sdfsdf</div>
-          <div className="">sdfsdf</div>
-          <div className="">sdfsdf</div>
         </div>
         <div
           className={
