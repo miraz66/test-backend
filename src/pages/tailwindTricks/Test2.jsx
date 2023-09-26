39187740a8d9562a8b91e0039d741c0c6b6c46ae
miraz66 <mirajulislam0509@gmail.com>
2023-09-26 23:52:05 +0600

    changeTest

--- src/pages/tailwindTricks/Test2.jsx
+++ src/pages/tailwindTricks/Test2.jsx
@@ -1,3 +1,35 @@
-export default function Test2() {
-  return;
+import PropTypes from "prop-types";
+
+import { useState } from "react";
+
+export default function Test2({ children }) {
+  const [show, setShow] = useState(true);
+
+  return (
+    <>
+      <div onClick={() => setShow(!show)} className="cursor-pointer">
+        click
+      </div>
+      <div className="h-screen bg-black overflow-y-hidden">
+        <div className="relative">
+          <div className="">sdfsdf</div>
+          <div className="">sdfsdf</div>
+          <div className="">sdfsdf</div>
+        </div>
+        <div
+          className={
+            !show
+              ? "bg-white h-screen absolute top-10 translate-y-full w-full ease-in-out duration-700 rounded-xl"
+              : "bg-white h-screen absolute top-10  w-full ease-in-out duration-700 translate-y-0 rounded-xl"
+          }
+        >
+          {children}
+        </div>
+      </div>
+    </>
+  );
 }
+
+Test2.propTypes = {
+  children: PropTypes.array.isRequired,
+};
