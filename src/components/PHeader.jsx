e002672f6dafbe215518c503d746b239d58719e0
miraz66 <mirajulislam0509@gmail.com>
2023-09-29 00:49:46 +0600

    ProtfolioHeader

--- src/components/PHeader.jsx
+++ src/components/PHeader.jsx
@@ -6,17 +6,17 @@ export default function PHeader() {
     <>
       <MainContainer>
         <HomeDisplay css="h-[42rem] lg:h-[50rem] xl:h-[48rem] relative rounded-t-[40px]">
-          <div className="absolute top-52 md:top-60 lg:top-72 xl:top-80 w-full">
+          <div className="absolute top-52 md:top-60 lg:top-72 xl:top-72 w-full">
             <div className="max-w-7xl mx-5 lg:mx-10 2xl:mx-auto">
               <div className="max-w-3xl mx-auto">
-                <span>HELLO, I'M</span>
-                <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl uppercase text-gray-800 font-semibold tracking-tight leading-10 lg:leading-[90px]">
+                <span className="text-neutral-600 font-bold">HELLO, I'M</span>
+                <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl uppercase text-neutral-800 font-semibold tracking-wide leading-10 lg:leading-[90px]">
                   Md.MIRAJUL ISLAM
                 </h1>
 
-                <p className="text-lg lg:text-xl pt-10 leading-8 text-gray-700">
+                <p className="text-lg lg:text-xl pt-10 text-neutral-60 font-medium">
                   Web Develops and front-end, Backend Developer Skills:
-                  CSS,React,Python,Django-rest-api
+                  CSS,React,Python, Django-rest-api
                 </p>
               </div>
             </div>
