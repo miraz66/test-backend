4cb25f59f381921f823dd406764dce29942a2843
miraz66 <mirajulislam0509@gmail.com>
2023-09-29 23:46:08 +0600

    ProtfolioHeader

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -42,6 +42,8 @@ export default function Test() {
           <div className="absolute top-96 left-0">dsfsdfs</div>
           <div className="absolute top-96 left-0">dsfsdfs</div>
           <div className="absolute top-96 left-0">dsfsdfs</div>
+          <div className="absolute top-96 left-0">dsfsdfs</div>
+          <div className="absolute top-96 left-0">dsfsdfs</div>
         </div>
 
         <div className="">
