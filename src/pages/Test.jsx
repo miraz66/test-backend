7a0251b548cbc4bdcf4c19ce034fcb251f2e49c4
miraz66 <mirajulislam0509@gmail.com>
2023-10-02 23:24:41 +0600

    Logic

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -141,6 +141,8 @@ export default function Test() {
         <div className=""></div>
         <div className=""></div>
         <div className=""></div>
+        <div className=""></div>
+        <div className=""></div>
       </Test2>
     </>
   );
