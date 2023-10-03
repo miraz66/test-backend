d82b68c9ef1dcd16c2253d004eaf9ee79885a8d3
miraz66 <mirajulislam0509@gmail.com>
2023-10-03 23:45:25 +0600

    NewTest

--- src/components/new/New.jsx
+++ src/components/new/New.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 export default function New() {
   return (
-    <div>
-      Innovative, task-driven professional with 8+ years of experience in web
-      design and development across diverse industries. Equipped with a record
-      of success in consistently identifying and providing the technological
-      needs of companies through ingenious innovation. Proficient in developing
-      databases, creating user interfaces, writing and testing codes,
-      troubleshooting simple/complex issues, and implementing new features based
-      on user feedback.
-    </div>
+    <>
+      <div className="">
+        {" "}
+        Innovative, task-driven professional with 8+ years of experience in web
+        design and development across diverse industries. Equipped with a record
+        of success in consistently identifying and providing the technological
+        needs of companies through ingenious innovation. Proficient in
+        developing databases, creating user interfaces, writing and testing
+        codes, troubleshooting simple/complex issues, and implementing new
+        features based on user feedback.
+      </div>
+    </>
   );
 }
