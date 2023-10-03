86b56edade27a2efe99103dc4626332edee0b35a
miraz66 <mirajulislam0509@gmail.com>
2023-10-03 23:49:54 +0600

    NewTest

--- src/components/new/New.jsx
+++ src/components/new/New.jsx
@@ -1,54 +1,5 @@
 import React from "react";
 
 export default function New() {
-  return (
-    <>
-      <div className="">
-        {" "}
-        Innovative, task-driven professional with 8+ years of experience in web
-        design and development across diverse industries. Equipped with a record
-        of success in consistently identifying and providing the technological
-        needs of companies through ingenious innovation. Proficient in
-        developing databases, creating user interfaces, writing and testing
-        codes, troubleshooting simple/complex issues, and implementing new
-        features based on user feedback. Innovative, task-driven professional
-        with 8+ years of experience in web design and development across diverse
-        industries. Equipped with a record of success in consistently
-        identifying and providing the technological needs of companies through
-        ingenious innovation. Proficient in developing databases, creating user
-        interfaces, writing and testing codes, troubleshooting simple/complex
-        issues, and implementing new features based on user feedback.
-      </div>{" "}
-      <div>
-        {" "}
-        Innovative, task-driven professional with 8+ years of experience in web
-        design and development across diverse industries. Equipped with a record
-        of success in consistently identifying and providing the technological
-        needs of companies through ingenious innovation. Proficient in
-        developing databases, creating user interfaces, writing and testing
-        codes, troubleshooting simple/complex issues, and implementing new
-        features based on user feedback.
-      </div>
-      <div>
-        {" "}
-        Innovative, task-driven professional with 8+ years of experience in web
-        design and development across diverse industries. Equipped with a record
-        of success in consistently identifying and providing the technological
-        needs of companies through ingenious innovation. Proficient in
-        developing databases, creating user interfaces, writing and testing
-        codes, troubleshooting simple/complex issues, and implementing new
-        features based on user feedback.
-      </div>
-      <div>
-        {" "}
-        Innovative, task-driven professional with 8+ years of experience in web
-        design and development across diverse industries. Equipped with a record
-        of success in consistently identifying and providing the technological
-        needs of companies through ingenious innovation. Proficient in
-        developing databases, creating user interfaces, writing and testing
-        codes, troubleshooting simple/complex issues, and implementing new
-        features based on user feedback.
-      </div>
-    </>
-  );
+  return <></>;
 }
