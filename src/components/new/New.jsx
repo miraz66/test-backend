df9f12a48f840381c920bc225c162672ca0799ca
miraz66 <mirajulislam0509@gmail.com>
2023-10-03 23:47:14 +0600

    NavBar

--- src/components/new/New.jsx
+++ src/components/new/New.jsx
@@ -64,6 +64,17 @@ export default function New() {
         codes, troubleshooting simple/complex issues, and implementing new
         features based on user feedback.
       </div>
+      #9D9D9D{" "}
+      <div>
+        {" "}
+        Innovative, task-driven professional with 8+ years of experience in web
+        design and development across diverse industries. Equipped with a record
+        of success in consistently identifying and providing the technological
+        needs of companies through ingenious innovation. Proficient in
+        developing databases, creating user interfaces, writing and testing
+        codes, troubleshooting simple/complex issues, and implementing new
+        features based on user feedback.
+      </div>
     </>
   );
 }
