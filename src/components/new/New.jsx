ad2dfd182cfe98648c092937185a76796ce319eb
miraz66 <mirajulislam0509@gmail.com>
2023-10-05 23:37:22 +0600

    Home Image

--- src/components/new/New.jsx
+++ src/components/new/New.jsx
@@ -55,6 +55,8 @@ export default function New() {
       <div className=""></div>
       <div className=""></div>
       <div className=""></div>
+      <div className=""></div>
+      <div className=""></div>
 
       <div className=""></div>
     </>
