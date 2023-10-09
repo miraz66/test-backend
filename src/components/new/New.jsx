f6b1cde0b448313ccd2aa352d96b761218f8287b
miraz66 <mirajulislam0509@gmail.com>
2023-10-09 23:47:06 +0600

    all

--- src/components/new/New.jsx
+++ src/components/new/New.jsx
@@ -4,6 +4,8 @@ export default function New() {
       <div className=""></div>
       <div className=""></div>
       <div className=""></div>
+      <div className=""></div>
+      <div className=""></div>
     </>
   );
 }
