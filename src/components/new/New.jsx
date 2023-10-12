32c5e5ca973b259564f926608606f196b256b3ac
miraz66 <mirajulislam0509@gmail.com>
2023-10-12 23:15:04 +0600

    studio Tailwind

--- src/components/new/New.jsx
+++ src/components/new/New.jsx
@@ -7,9 +7,6 @@ export default function New() {
       <div className=""></div>
       <div className=""></div>
       <div className=""></div>
-      <div className=""></div>
-      <div className=""></div>
-      <div className=""></div>
     </>
   );
 }
