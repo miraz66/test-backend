03171e247270f4dd268fba122165974430c8e849
miraz66 <mirajulislam0509@gmail.com>
2023-10-12 23:17:50 +0600

    Commant

--- src/components/new/New.jsx
+++ src/components/new/New.jsx
@@ -36,6 +36,11 @@ export default function New() {
         I am a full-stack developer with a focus on project analysis and design,
         using various technologies to bring my designs and initial ideas to
         life.
+      </div>{" "}
+      <div className="">
+        I am a full-stack developer with a focus on project analysis and design,
+        using various technologies to bring my designs and initial ideas to
+        life.
       </div>
     </>
   );
